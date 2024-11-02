5dc88de9ad8398e90d5c0b502477eb8e6d3dcfb2
Jan Atle Ramsli <jaramsli@gmail.com>
2024-11-02 10:42:16 +0100

    Count tasks

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/cFiles/main.c to edit this template
  */
 
-/* 
+/*
  * File:   main.c
  * Author: atle
  *
@@ -13,66 +13,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <unistd.h>
 
 char *primes;
-int nprimes;
+long nprimes;
 
-static int semF = 0; // no go
+int noTasks = 0; // no go
 
 /**
  *   populate the array from prime p
  **/
 void *mark_from(void *p) {
-	int n = *(int*) p;
-	primes[n] = 'p';
-	semF = 1;	// We have marked our first spot so the next thread can be started
-	for (int i = n*2; i < nprimes; i+=n) {
-		primes[i] = 'n';
-	}
+    long n = *(long*) p;
+    primes[n] = 'p';
+    ++noTasks;
+    for (long i = n * 2; i < nprimes; i += n) {
+        primes[i] = 'n';
+    }
+    --noTasks;
 }
 
 void print_primes() {
 
-	for (int i = 0; i < nprimes; i++) {
-		if (primes[i] == 'p') {
-			printf("%d ", i);
-		} else if (primes[i] == 'n') {
-			// printf("%d,", i);
-		} else {
-			printf("%d ", i);
-		}
-	}
-
+    for (long i = 0; i < nprimes; i++) {
+        if (primes[i] == 'p') {
+            printf("\n%ld ", i);
+        } else if (primes[i] == 'n') {
+            // printf("%d,", i);
+        } else {
+            //printf("\n%d not initialized\n", i);
+        }
+    }
 }
 
 /*
- * 
+ *
  */
 int main(int argc, char** argv) {
-	pthread_t tid;
-	if (argc < 2) {
-		nprimes = 121;
-	} else {
-		nprimes = atoi(argv[1]);
-	}
-	primes = malloc(nprimes * sizeof (char));
-	int first = 2;
-	pthread_create(&tid, NULL, mark_from, (void *) (&first));
-	for(int i = 3; i < (nprimes/2); i++){
-		if (primes[i]==0){
-			pthread_t tid2;
-			semF = 0; // Set semaphore
-			int j = i;
-			pthread_create(&tid2, NULL, mark_from, (void *) (&j));
-		}
-		while(semF==0){
-			; //printf("."); // Wait until thread clears semaphore
-		}
-		
-	}
-	pthread_join(tid, NULL);
-	print_primes();
-	free(primes);
-	return (EXIT_SUCCESS);
-}
+    pthread_t tid;
+    if (argc < 2) {
+        nprimes = 10000000;
+    } else {
+        nprimes = atoi(argv[1]);
+    }
+    printf("Looking for primes up to %ld\n", nprimes);
+    noTasks = 0;
+    primes = malloc(nprimes * sizeof (char));
+    long first = 2;
+    pthread_create(&tid, NULL, mark_from, (void *) (&first));
+    usleep(30);
+    for (long i = 3; i < (nprimes); i++) {
+        while (noTasks > 56) {
+            printf("\r%d tasks active ", noTasks);
+            usleep(30);
+            noTasks--;
+        }
+        if (primes[i] == 0) {
+            pthread_t tid2;
+            long j = i;
+            int fail = pthread_create(&tid2, NULL, mark_from, (void *) (&j));
+        }
+    }
+    pthread_join(tid, NULL);
+    print_primes();
+    free(primes);
+    return (EXIT_SUCCESS);
 
+}
